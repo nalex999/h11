76414b55d5e08abb1a59d2dec8d5141d47ddb4d8
nalex999 <nalex888@mail.ru>
2017-04-21 22:56:33 +0700

    Update form1.cs

--- form1.cs
+++ form1.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Text;uol;gul.l.yilyigl.illu;;i
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
